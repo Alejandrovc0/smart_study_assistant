56fe83a8dae66562094f22f20344260d4e5e4c26
Alejandrovc0 <137929379+Alejandrovc0@users.noreply.github.com>
2024-07-10 21:16:12 -0400

    Add backend server

---
+++ backend/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from backend.langgraph_agent import MainAgent
+
+backend_app = Flask(__name__)
+
+
+@backend_app.route("/", methods=["GET"])
+def index():
+    return jsonify({"status": "Running"}), 200
+
+
+@backend_app.route("/smart_study_assistant", methods=["POST"])
+def generate_newspaper():
+    data = request.json
+    main_agent = MainAgent()
+    study_assistant = main_agent.run(data["topics"])
+    return jsonify({"path": study_assistant}), 200
